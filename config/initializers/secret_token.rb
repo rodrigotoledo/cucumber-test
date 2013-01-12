66317044f8997dc7b1d1cac9ea24c47142f506d1
Rodrigo Toledo <agc.rodrigo@gmail.com>
2013-01-11 23:01:05 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TesteCucumber::Application.config.secret_token = 'c0482f72934d260720979789a41ac1c1910c67234edc3f133309d36e83ff89defd6bf5f190a6610fa4ad686136769456bb4a7e9613eefae52e488ac030ac593b'
